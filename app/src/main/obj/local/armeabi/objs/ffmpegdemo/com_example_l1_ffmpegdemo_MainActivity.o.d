ccbfa27102fbb50513111e1e8e8e69754537110e
L1 <getnway@gmail.com>
2015-12-13 20:08:54 +0800

    first commit

---
+++ app/src/main/obj/local/armeabi/objs/ffmpegdemo/com_example_l1_ffmpegdemo_MainActivity.o.d
@@ -0,0 +1,284 @@
+obj/local/armeabi/objs/ffmpegdemo/com_example_l1_ffmpegdemo_MainActivity.o: \
+ jni/com_example_l1_ffmpegdemo_MainActivity.c \
+ jni/com_example_l1_ffmpegdemo_MainActivity.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ jni/include/libavcodec/avcodec.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ jni/include/libavcodec/../libavutil/samplefmt.h \
+ jni/include/libavcodec/../libavutil/avutil.h \
+ jni/include/libavcodec/../libavutil/common.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/inttypes.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/math.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ jni/include/libavcodec/../libavutil/attributes.h \
+ jni/include/libavcodec/../libavutil/version.h \
+ jni/include/libavcodec/../libavutil/macros.h \
+ jni/include/libavcodec/../libavutil/../libavutil/avconfig.h \
+ jni/include/libavcodec/../libavutil/mem.h \
+ jni/include/libavcodec/../libavutil/error.h \
+ jni/include/libavcodec/../libavutil/rational.h \
+ jni/include/libavcodec/../libavutil/mathematics.h \
+ jni/include/libavcodec/../libavutil/intfloat.h \
+ jni/include/libavcodec/../libavutil/log.h \
+ jni/include/libavcodec/../libavutil/pixfmt.h \
+ jni/include/libavcodec/../libavutil/attributes.h \
+ jni/include/libavcodec/../libavutil/avutil.h \
+ jni/include/libavcodec/../libavutil/buffer.h \
+ jni/include/libavcodec/../libavutil/cpu.h \
+ jni/include/libavcodec/../libavutil/channel_layout.h \
+ jni/include/libavcodec/../libavutil/dict.h \
+ jni/include/libavcodec/../libavutil/frame.h \
+ jni/include/libavcodec/../libavutil/buffer.h \
+ jni/include/libavcodec/../libavutil/dict.h \
+ jni/include/libavcodec/../libavutil/samplefmt.h \
+ jni/include/libavcodec/../libavutil/log.h \
+ jni/include/libavcodec/../libavutil/pixfmt.h \
+ jni/include/libavcodec/../libavutil/rational.h \
+ jni/include/libavcodec/version.h \
+ jni/include/libavcodec/../libavutil/version.h \
+ jni/include/libavfilter/avfilter.h \
+ jni/include/libavfilter/../libavutil/attributes.h \
+ jni/include/libavfilter/../libavutil/avutil.h \
+ jni/include/libavfilter/../libavutil/dict.h \
+ jni/include/libavfilter/../libavutil/frame.h \
+ jni/include/libavfilter/../libavutil/log.h \
+ jni/include/libavfilter/../libavutil/samplefmt.h \
+ jni/include/libavfilter/../libavutil/pixfmt.h \
+ jni/include/libavfilter/../libavutil/rational.h \
+ jni/include/libavfilter/../libavfilter/version.h \
+ jni/include/libavfilter/../libavfilter/../libavutil/version.h \
+ jni/include/libavformat/avformat.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/time.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/include/libavformat/../libavcodec/avcodec.h \
+ jni/include/libavformat/../libavutil/dict.h \
+ jni/include/libavformat/../libavutil/log.h \
+ jni/include/libavformat/avio.h \
+ jni/include/libavformat/../libavutil/common.h \
+ jni/include/libavformat/../libavformat/version.h \
+ jni/include/libavformat/../libavformat/../libavutil/version.h \
+ E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h
+
+jni/com_example_l1_ffmpegdemo_MainActivity.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+jni/include/libavcodec/avcodec.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+jni/include/libavcodec/../libavutil/samplefmt.h:
+
+jni/include/libavcodec/../libavutil/avutil.h:
+
+jni/include/libavcodec/../libavutil/common.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/inttypes.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/math.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+jni/include/libavcodec/../libavutil/attributes.h:
+
+jni/include/libavcodec/../libavutil/version.h:
+
+jni/include/libavcodec/../libavutil/macros.h:
+
+jni/include/libavcodec/../libavutil/../libavutil/avconfig.h:
+
+jni/include/libavcodec/../libavutil/mem.h:
+
+jni/include/libavcodec/../libavutil/error.h:
+
+jni/include/libavcodec/../libavutil/rational.h:
+
+jni/include/libavcodec/../libavutil/mathematics.h:
+
+jni/include/libavcodec/../libavutil/intfloat.h:
+
+jni/include/libavcodec/../libavutil/log.h:
+
+jni/include/libavcodec/../libavutil/pixfmt.h:
+
+jni/include/libavcodec/../libavutil/attributes.h:
+
+jni/include/libavcodec/../libavutil/avutil.h:
+
+jni/include/libavcodec/../libavutil/buffer.h:
+
+jni/include/libavcodec/../libavutil/cpu.h:
+
+jni/include/libavcodec/../libavutil/channel_layout.h:
+
+jni/include/libavcodec/../libavutil/dict.h:
+
+jni/include/libavcodec/../libavutil/frame.h:
+
+jni/include/libavcodec/../libavutil/buffer.h:
+
+jni/include/libavcodec/../libavutil/dict.h:
+
+jni/include/libavcodec/../libavutil/samplefmt.h:
+
+jni/include/libavcodec/../libavutil/log.h:
+
+jni/include/libavcodec/../libavutil/pixfmt.h:
+
+jni/include/libavcodec/../libavutil/rational.h:
+
+jni/include/libavcodec/version.h:
+
+jni/include/libavcodec/../libavutil/version.h:
+
+jni/include/libavfilter/avfilter.h:
+
+jni/include/libavfilter/../libavutil/attributes.h:
+
+jni/include/libavfilter/../libavutil/avutil.h:
+
+jni/include/libavfilter/../libavutil/dict.h:
+
+jni/include/libavfilter/../libavutil/frame.h:
+
+jni/include/libavfilter/../libavutil/log.h:
+
+jni/include/libavfilter/../libavutil/samplefmt.h:
+
+jni/include/libavfilter/../libavutil/pixfmt.h:
+
+jni/include/libavfilter/../libavutil/rational.h:
+
+jni/include/libavfilter/../libavfilter/version.h:
+
+jni/include/libavfilter/../libavfilter/../libavutil/version.h:
+
+jni/include/libavformat/avformat.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/time.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/include/libavformat/../libavcodec/avcodec.h:
+
+jni/include/libavformat/../libavutil/dict.h:
+
+jni/include/libavformat/../libavutil/log.h:
+
+jni/include/libavformat/avio.h:
+
+jni/include/libavformat/../libavutil/common.h:
+
+jni/include/libavformat/../libavformat/version.h:
+
+jni/include/libavformat/../libavformat/../libavutil/version.h:
+
+E:/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
